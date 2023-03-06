525e89920e6c4f99244ce067c2c30d5909278430
Andrey Linnik <av.linnik79@ya.ru>
2023-03-06 11:42:21 +0300

    add some classes

---
+++ Task7.7/Delivery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._7
+{
+    internal class Delivery
+    {
+        private string Adress { get; set; }
+    }
+}
